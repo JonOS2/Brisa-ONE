038801c437ff07df9bd100147bb5a08677ab021b
Athisson Alberto <athissonlm17@gmail.com>
2026-05-08 09:59:26 -0300

    planilha pessoas csv -> xlsx

--- BRISA-FRONTEND/src/components/figma-generated/pages/pessoas.vue
+++ BRISA-FRONTEND/src/components/figma-generated/pages/pessoas.vue
@@ -11,14 +11,19 @@
           </div>
 
           <div class="top-actions">
-            <button type="button" class="ghost-btn" @click="downloadTemplate">
+            <a 
+              href="/Modelo_Planilha_Pessoas.xlsx" 
+              download="Modelo_Planilha_Pessoas.xlsx" 
+              class="ghost-btn" 
+              style="text-decoration: none;"
+            >
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                 <polyline points="7 10 12 15 17 10"></polyline>
                 <line x1="12" y1="15" x2="12" y2="3"></line>
               </svg>
               Modelo de planilha
-            </button>
+            </a>
 
             <button type="button" class="ghost-btn" @click="showCreateModal = true">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
@@ -1376,13 +1381,6 @@ const openMoreActions = () => {
 
 const downloadTemplate = () => {
   showTemplateModal.value = true;
-};
-
-const downloadTemplateXlsx = () => {
-  const worksheet = XLSX.utils.aoa_to_sheet([templateColumns]);
-  const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Modelo');
-  XLSX.writeFile(workbook, 'modelo-planilha-alunos.xlsx');
 };
 
 const formatCPFInput = (event) => {
